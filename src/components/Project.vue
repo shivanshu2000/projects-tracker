d3cfd1fc65f5ee6304b7fd072967d20362dac606
Shivanshu2000 <shivanshusr82@gmail.com>
2021-07-19 17:38:09 +0530

    repo init

---
+++ src/components/Project.vue
@@ -0,0 +1,122 @@
+<template>
+<div class="project">
+  <div class="actions">
+  <div class="left__section">
+  <h3 @click="toggleDetails">{{project.title}}</h3>
+  <span @click="this.showDetails = !this.showDetails" :class="{rotate: showDetails}" class="material-icons down" >
+expand_more
+</span>
+
+  </div>
+  <div class="icons">
+  <span class="material-icons">edit</span>
+  <span @click="deleteProject" class="material-icons">delete</span>
+  <span class="material-icons">done</span>
+  </div>
+  </div>
+  <div class="divider" :class="{show__divider: showDetails}"/>
+  <div class="details" :class="{drop:showDetails}">
+  <p>{{project.details}}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'project',
+    props:['project'],
+    data(){
+      return{
+        showDetails:false,
+      }
+    },
+
+    methods:{
+        toggleDetails(){
+          this.showDetails = !this.showDetails
+        },
+
+        deleteProject(){
+          this.$emit('delete-project', this.project.id)
+        }
+    }
+}
+</script>
+
+<style>
+.project{
+  margin: 20px auto;
+  background: white;
+  padding: 9px 0px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+  border-left:4px solid #e90874; 
+  overflow:hidden
+}  
+
+.details{
+    max-height:0px;
+    visibility: hidden;
+    padding:0 1rem; 
+    transition: max-height 0.65s ease-in-out, visibility 0.51s ease-in-out 0.15s;
+}
+
+/* .drop{
+    transition: height 3s linear;
+
+} */
+
+.details.drop{
+    height:auto;
+    max-height:900px;
+    visibility:visible
+}
+
+h3{
+  cursor:pointer;
+  text-align:center 
+
+}
+
+.actions{
+    padding: 0 15px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    flex-wrap:wrap
+}
+
+.down{
+    color: black;
+    transition: transform 0.3s ease-in-out
+}
+
+.down.rotate{
+  transform: rotate(180deg)
+}
+
+.left__section{
+    flex:1;
+    padding-right:19px;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff
+}
+
+.material-icons{
+    cursor:pointer;
+    font-size: 24px;
+    margin-left:9px;
+    color:#ccc;
+}
+
+.icons{
+   margin-left:1.5rem
+
+}
+
+.material-icons:hover{
+    color: #bbb
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div v-if="projects.length">
+   <div v-for="project in projects" :key="project.id">
+     <Project @delete-project="deleteProject" :project="project"/>
+   </div> 
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Project from '@/components/Project.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      projects:[
+         {
+            id:1,
+            title:"Title 1",
+            details:"This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 This is title 1 "
+        },
+        {
+            id:2,
+            title:"Title 2",
+            details:"This is title 2"
+
+        }
+      ]
+    }
+  },
+
+  components:{
+    Project
+  },
+
+  methods:{
+    deleteProject(id){
+      this.projects = this.projects.filter(project=> project.id!==id)
+    }
   }
 }
 </script>
